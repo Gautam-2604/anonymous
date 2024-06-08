cbbdaf6b07ccb4009ea71f957727a00be88837e0
Gautam-2604 <gaurisariag@gmail.com>
2024-06-08 12:28:16 +0530

    Code verification route completed

--- src/app/api/verify-code/route.ts
+++ src/app/api/verify-code/route.ts
@@ -2,5 +2,47 @@ import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 
 export async function POST(request: Request){
-    
+    await dbConnect()
+    try {
+        const {username, code} = await request.json()
+        const decodedUsername = decodeURIComponent(username)
+        const user = await UserModel.findOne({username: decodedUsername})
+        if(!user){
+           return Response.json({
+               success: false,
+               message:"User does not exist"
+           },{status:404})
+        }
+
+        const isCodeValid = user.verifyCode === code
+        const isCodeNotExpired = new Date(user.verifyCodeExpiry)> new Date()
+
+        if(isCodeValid && isCodeNotExpired){
+            user.isVerified= true
+            await user.save()
+            return Response.json({
+                success: true,
+                message:"Code verified"
+            },{status:200})
+        }
+        else if(!isCodeNotExpired){
+            return Response.json({
+                success: false,
+                message:"Code expired"
+            },{status:404})
+        }else{
+            return Response.json({
+                success: false,
+                message:"Wrong code"
+            },{status:404})
+        }
+
+
+    } catch (error) {
+        return Response.json({
+            success: false,
+            message:"Error in verifying code"
+        },{status:500})
+        
+    }
 }
