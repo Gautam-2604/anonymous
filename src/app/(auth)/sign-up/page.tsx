0f52d70485bb4774b463ebb35bae04968e2d1383
Gautam-2604 <gaurisariag@gmail.com>
2024-06-10 10:21:47 +0530

    some errors rectified

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -5,7 +5,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import {useDebounce} from 'usehooks-ts'
+import {useDebounceValue} from 'usehooks-ts'
 import * as z from 'zod';
 
 import { Button } from '@/components/ui/button';
@@ -28,7 +28,7 @@ export default function SignUpForm() {
   const [usernameMessage, setUsernameMessage] = useState('');
   const [isCheckingUsername, setIsCheckingUsername] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const debouncedUsername = useDebounce(username, 300);
+  const debouncedUsername = useDebounceValue(username,300) 
 
   const router = useRouter();
   const { toast } = useToast();
@@ -102,7 +102,7 @@ export default function SignUpForm() {
       <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
         <div className="text-center">
           <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
-            Join True Feedback
+            Join Anonymous
           </h1>
           <p className="mb-4">Sign up to start your anonymous adventure</p>
         </div>
